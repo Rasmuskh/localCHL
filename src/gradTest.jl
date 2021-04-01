804c3494073e65a75ace73f30e4fb15ba4fbaaa0
Rasmus <rasmasmus@gmail.com>
2021-04-01 14:40:20 +0200

    infering optimal beta works now. added printing of mean and std of betas

--- src/gradTest.jl
+++ src/gradTest.jl
@@ -31,23 +31,27 @@ function getdata(batchsize, trainsamples, testsamples)
     return train_loader, test_loader
 end
 
+"""Probably not needed anymore"""
 function lifted_E_layer1(x, z, W0, b0)
     a = W0*x .+ b0 .- z
     return BLAS.dot(a,a)
 end
 
+"""Probably not needed anymore"""
 function lifted_loss_layer1(x, z_hat, z_free, W0, b0, β)
     E_hat = lifted_E_layer1(x, z_hat, W0, b0)
     E_free = lifted_E_layer1(x, z_free, W0, b0)
     return (E_hat - E_free)/β
 end
 
+"""Probably not needed anymore"""
 # function loss(x, y)
 #     pred = Net(x)
 #     mse(y, pred)
 #     #sum((target .- y).^2)
 # end
 
+"""Probably not needed anymore"""
 # function loss_2nd_layer_onwards(z, y)
 #     pred = Net[2:end](z)
 #     mse(y, pred)
@@ -71,7 +75,7 @@ function loss_and_accuracy(data_loader, Net)
     return ls / num, acc / num
 end
 
-function train_BP(Net, η, batchsize, opt, nEpochs, trainsamples, testsamples)
+function train_BP(Net, batchsize, opt, nEpochs, trainsamples, testsamples)
 
     train_loader, test_loader = getdata(batchsize, trainsamples, testsamples)
 
@@ -97,6 +101,7 @@ function train_BP(Net, η, batchsize, opt, nEpochs, trainsamples, testsamples)
     return Net
 end
 
+"""Probably not needed anymore"""
 function maximize_gap_batch(x, ∇z1_free, W0, b0, β0, βmax, E_z1_free, activation)
     β = β0
     β_best = 0
@@ -121,39 +126,14 @@ function maximize_gap_batch(x, ∇z1_free, W0, b0, β0, βmax, E_z1_free, activa
     return (β_best, z1_hat_best)
 end
 
-# function maximize_gap_xi(X, ∇z1_free, W0, b0, β0, βmax, E_z1_free, activation)
-#     xArray = [xi for xi in eachcol(X)]
-#     for x in xArray
-#         β = β0
-#         β_best = 0
-#         ΔE_best = 0
-#         #println(b0)
-#         z1_hat_best = similar(b0) #z1 has the same shape size as b0
-#         i = 0
-#         while β<βmax
-#             i += 1
-#             z1_hat = get_z1_hat(x, W0, b0, β, ∇z1_free, activation)
-#             E_z1_hat = lifted_E_layer1(x, z1_hat, W0, b0)
-#             ΔE = (E_z1_hat - E_z1_free)/β #  E_z1_free not really necessary here as it is constant!
-#             if (ΔE > ΔE_best) || (i==1)
-#                 ΔE_best = ΔE
-#                 β_best = β
-#                 z1_hat_best = z1_hat
-#             elseif ΔE<ΔE_best
-#                 break
-#             end
-#             β *= 2
-#         #get z1_hat[i] by finiding optimal β
-#         #get ∇θ[1:2] from β, z1_hat and z1_free
-
+"""Probably not needed anymore"""
 function get_z1_hat(x, W0, b0, β, ∇z1_free, activation)
     z1_hat = activation.(W0*x .+b0 .-β*∇z1_free)
     return z1_hat
 end
 
+"""Probably not needed anymore"""
 function get_∇θ1(x, z1_hat, z1_free, W0, b0, β, batchsize)
-    #println("z1_free", size(z1_free))
-    #println("z1_hat", size(z1_hat))
     ∇b1 = (z1_free .- z1_hat)/β
     ∇W1 = (∇b1*x')
 
@@ -173,48 +153,56 @@ function f(X, z1_free, ∇z1_free, W1, b1, activation)
     ∇W1_batch = [similar(W1) for k=1:batchsize]
     ∇b1_batch = [similar(b1) for k=1:batchsize]
 
-
+    β_best_Arr = [ones(dType, size(b1)) for k=1:batchsize]
 
     Threads.@threads for i=1:batchsize # Loop over datapoints
+
+        # Precompute preactivations
         a = W1*x_batch[i] .+ b1
+
         for n=1:length(b1) # Loop over neurons
-            β = 0.1; β_best = β
+            β = 1.0;  β_best_Arr[i][n] = β
             z1_hat_batch[i][n] = activation(a[n] - β*∇z1_free_batch[i][n])
             E1_hat = ((a[n] - z1_hat_batch[i][n])^2)
             E1_free = ((a[n] - z1_free_batch[i][n])^2)
             ΔE_max = (E1_hat - E1_free)/β
+
             #Choose β to maximize ΔE = E_hat - E_free
-            for t=1:12
+            for t=1:8
+                β *= 2
                 z_dummy = activation(a[n] - β*∇z1_free_batch[i][n])
                 E1_hat = ((a[n] - z1_hat_batch[i][n])^2)
                 E1_free = ((a[n] - z1_free_batch[i][n])^2)
                 ΔE = (E1_hat - E1_free)/β
-                if ΔE>ΔE_max
+                if ΔE>=ΔE_max
                     ΔE_max = ΔE
                     z1_hat_batch[i][n] = z_dummy
-                    β_best = β
+                    β_best_Arr[i][n] = β
                 else
                     break
                 end
-                β *= 2
+
             end
-            ∇b1_batch[i][n] =  (z1_free_batch[i][n] .- z1_hat_batch[i][n])/β
+            #println(β_best)
+            #z1_hat_batch[i][n] = activation(a[n] - 100.0*∇z1_free_batch[i][n])
+            ∇b1_batch[i][n] =  (z1_free_batch[i][n] .- z1_hat_batch[i][n])/β_best_Arr[i][n]
             ∇W1_batch[i][n,:] =  ∇b1_batch[i][n]*x_batch[i]'
         end
     end
+    β_av = sum(reduce(hcat,β_best_Arr))/length(b1)
+    β_std = std(reduce(hcat,β_best_Arr))/length(b1)
     ∇W1 = mean(∇W1_batch)
     ∇b1 = mean(∇b1_batch)
-    
 
-    # β = 1.0
+    # β = 4096.0
     # z1_hat = get_z1_hat(X, W1, b1, β, ∇z1_free, activation)
     # ∇b1, ∇W1 = get_∇θ1(X, z1_hat, z1_free, W1, b1, β, batchsize)
 
-    return (∇b1, ∇W1)
+    return (∇b1, ∇W1, β_av, β_std)
 end
 
 
-function train_hybrid(Net, η, batchsize, opt, nEpochs,  trainsamples, testsamples)
+function train_hybrid(Net, batchsize, opt, nEpochs,  trainsamples, testsamples)
 
     train_loader, test_loader = getdata(batchsize, trainsamples, testsamples)
 
@@ -224,20 +212,25 @@ function train_hybrid(Net, η, batchsize, opt, nEpochs,  trainsamples, testsampl
     # Training
     for epoch in 1:nEpochs
         t1 = time()
+        β_av = 0.0
+        β_std = 0.0
         for (x, y) in train_loader
+
             z1_free = Net[1](x)
             ∇θ = gradient(() -> mse(Net(x), y), θ) # compute gradient
 
             grad = gradient(() -> mse(Net[2:end](z1_free), y), params([z1_free]))
             ∇z1_free = grad[z1_free]
-            ∇b1, ∇W1 = f(x, z1_free, ∇z1_free, θ[1], θ[2], activation)
+            ∇b1, ∇W1, β_av_batch, β_std_batch = f(x, z1_free, ∇z1_free, θ[1], θ[2], activation)
+            β_av += β_av_batch
+            β_std += β_std_batch
 
             ∇θ[θ[1]][:,:] = ∇W1
             ∇θ[θ[2]][:] = ∇b1
             Flux.Optimise.update!(opt, θ, ∇θ) # update parameters
         end
-
-        
+        β_av /= trainsamples # get average batch mean(β)
+        β_std /= trainsamples # get average batch std(β)
         # Report on train and test
         train_loss, train_acc = loss_and_accuracy(train_loader, Net)
         test_loss, test_acc = loss_and_accuracy(test_loader, Net)
@@ -245,50 +238,55 @@ function train_hybrid(Net, η, batchsize, opt, nEpochs,  trainsamples, testsampl
         println("Epoch=$epoch")
         println("  train_loss = $train_loss, train_accuracy = $train_acc")
         println("  test_loss = $test_loss, test_accuracy = $test_acc")
+        println("  β_av = $β_av, β_std = $β_std")
         println("  Runtime: $(t2-t1)")
     end
     return Net
 end
 
+# Set datatype
+dType = Float32
+
 # Make network(s)
-nNeurons = [784, 64, 64 ,10]
+nNeurons = [784, 64, 64, 10]
 nLayers = length(nNeurons) - 1
 activation = [relu, relu, identity]
 dummyArray = [Dense(nNeurons[i], nNeurons[i+1], activation[i]) for i=1:nLayers] #Array of the Dense layers
 Net0 = Chain(dummyArray...) # Splat the layers and Chain them
-params(Net0)[1][:,:] -= 0.02*abs.(randn(64, 784))
+params(Net0)[1][:,:] .-= 0.03#-= 0.04*abs.(randn(64, 784))
 #params(Net0)[2][:,:] -= 2.5*ones(Float32, nNeurons[2])
 Net_BP = deepcopy(Net0)
 Net_hybrid = deepcopy(Net0)
 
 # Hyper parameters
-nEpochs = 2
-batchsize = 64
-η = 0.5
+nEpochs = 10
+batchsize = 16
+ηAdam = 0.0001
+ηSGD = 0.5
 ## Optimizer
-opt = Descent(η) #SGD
-#opt = ADAM(η)
+#opt = Descent(ηSGD) #SGD
+opt = ADAM(ηAdam)
 
 # Data
-trainsamples = 10000
+trainsamples = 60000
 testsamples = 10000
 
-LinearAlgebra.BLAS.set_num_threads(8)
-nT = ccall((:openblas_get_num_threads64_, Base.libblas_name), Cint, ())
-println(nT)
+#LinearAlgebra.BLAS.set_num_threads(8)
+#nT = ccall((:openblas_get_num_threads64_, Base.libblas_name), Cint, ())
+#println(nT)
 
 
 # Train hybrid network
 println("Training in hybrid-mode")
-Random.seed!(3323)
-train_hybrid(Net_hybrid, η, batchsize, opt, nEpochs, trainsamples, testsamples)
+Random.seed!(323)
+train_hybrid(Net_hybrid, batchsize, opt, nEpochs, trainsamples, testsamples)
 println("\nTraining finished!\n")
 
 # Train BP network
 println("Training in pure BP-mode")
-Random.seed!(3323)
-train_BP(Net_BP, η, batchsize, opt, nEpochs, trainsamples, testsamples)
-println("\nTraining finished!\n")
+Random.seed!(323)
+train_BP(Net_BP, batchsize, opt, nEpochs, trainsamples, testsamples)
+
 
 
 
