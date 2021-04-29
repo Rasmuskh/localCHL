b5d3b7ff9b73f4325d5bb17d1243005ffe9efa1a
Rasmus <rasmasmus@gmail.com>
2021-04-29 23:27:48 +0200

    deleted unused functions

--- src/utilityFunctions.jl
+++ src/utilityFunctions.jl
@@ -1,13 +1,11 @@
-dType = Float32
-
 mutable struct Net_CHL
     nNeurons::Array{Int32, 1}
 	  nLayers::Int32
-	  w::Array{Array{dType, 2},1}
-	  b::Array{Array{dType, 1},1}
-    highLim::Array{dType,1}
+	  w::Array{Array{Float32, 2},1}
+	  b::Array{Array{Float32, 1},1}
+    highLim::Array{Float32,1}
 	  num_updates::Int32
-	  History::Dict{String, Array{dType,1}}
+	  History::Dict{String, Array{Float32,1}}
 end
 
 
@@ -15,26 +13,20 @@ function init_network(nNeurons, highLim, init_mode="glorot_normal")
 	  """Initialize network weights and allocate arrays for training metric history."""
 	  nLayers = length(nNeurons) - 1
     if init_mode == "glorot_normal"
-        w = [1/sqrt(nNeurons[i] + nNeurons[i+1]) * randn(rng, dType, (nNeurons[i+1], nNeurons[i])) for i = 1:nLayers]
+        w = [1/sqrt(nNeurons[i] + nNeurons[i+1]) * randn(rng, Float32, (nNeurons[i+1], nNeurons[i])) for i = 1:nLayers]
     elseif init_mode == "glorot_uniform"
-        w = [(rand(rng, dType, (nNeurons[i+1], nNeurons[i])) .- 0.5) * sqrt(6.0/(nNeurons[i] + nNeurons[i+1])) for i = 1:nLayers]
+        w = [(rand(rng, Float32, (nNeurons[i+1], nNeurons[i])) .- 0.5) * sqrt(6.0/(nNeurons[i] + nNeurons[i+1])) for i = 1:nLayers]
     end
 
-	  b = [zeros(dType, (nNeurons[i+1])) for i = 1:nLayers]
+	  b = [zeros(Float32, (nNeurons[i+1])) for i = 1:nLayers]
 
 	  num_updates = 0
     # Network history dictionary
 	  History = Dict(
-	      "runTimes" => dType[],
-	      "acc_train" => dType[],
-	      "acc_test" => dType[],
-	      "J" => dType[],
-        "z1_sat_up" => dType[],
-        "z1_sat_down" => dType[],
-        "z2_sat_up" => dType[],
-        "z2_sat_down" => dType[],
-        "z3_sat_up" => dType[],
-        "z3_sat_down" => dType[]
+	      "runTimes" => Float32[],
+	      "acc_train" => Float32[],
+	      "acc_test" => Float32[],
+	      "J" => Float32[],
     )
 
 	  # Network struct
@@ -55,22 +47,6 @@ function LPOM_to_Flux(Net, activation)
 
     return fluxNet
 end
-        
-
-
-# """ReLU function: Clamps input between 0 and infinity"""
-# function ReLU(PreActivation)
-#     return max(0, PreActivation)
-# end
-
-# """Hard sigmoid function: Clamps input between 0 and 1"""
-# function HardSigmoid(PreActivation)
-#     return min.(1, max.(0, PreActivation))
-# end
-
-function Linear(PreActivation)
-    return PreActivation
-end
 
 """Clamp inputs. if low=0 and high=Inf then you have ReLU.
 If low=0 and high=Inf, then you have Hard Sigmoid."""
@@ -78,42 +54,16 @@ function Clamp(z, low=0.0, high=Inf)
     return min(high, max(low, z))
 end
 
-"""Simple feed-forward pass"""
-function forward(z, Net, activation, w1x_plus_b1)
-    z[1] = activation[1].(w1x_plus_b1)
-
-    @inbounds for i = 2:Net.nLayers-1
-        z[i] =  activation[i].(Net.w[i] * z[i-1] .+ Net.b[i])
-    end
-
-    #The final layer is linear
-    L = Net.nLayers
-    z[L] = activation[L].(Net.w[L] * z[L-1] .+ Net.b[L])
-    return z
-end
-
-"""Simple feed-forward pass"""
+"""Feed-forward pass"""
 function forward!(z, Net, activation, w1x_plus_b1)
     z[1] = activation[1].(w1x_plus_b1)
 
-    @inbounds for i = 2:Net.nLayers-1
+    for i = 2:Net.nLayers-1
         z[i] =  activation[i].(Net.w[i] * z[i-1] .+ Net.b[i])
     end
 
-    #The final layer is linear
     L = Net.nLayers
     z[L] = activation[L].(Net.w[L] * z[L-1] .+ Net.b[L])
-end
-
-function meanOfGrad(∇w, ∇w_batch, batchsize) 
-    ∇w .= ∇w_batch[1]
-    @inbounds for grad in ∇w_batch[2:end]
-        @inbounds for (layer, gradLayer) in enumerate(grad)
-            ∇w[layer] .+= gradLayer
-        end
-    end
-    ∇w ./= batchsize
-    return ∇w
 end
 
 function update_weights_ADAM(Net, η, ∇w, ∇b, M_w, M_b, V_w, V_b)
